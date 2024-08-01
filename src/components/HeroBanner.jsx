14994a6e71c43f59d56708127862ddf74795f113
Omkesh Jadhav <iamomkeshjadhav@gmail.com>
2024-08-01 08:08:49 +0530

    Data fetch for Search Exercises

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -4,6 +4,7 @@ import HeroBannerImage from "../assets/images/banner.png";
 const HeroBanner = () => {
   return (
     <div className="p-5 relative mt-[100px] ml-24 w-screen">
+      {/* Herobanner Text Content and Button */}
       <p className="text-[#FF2625] text-3xl font-extrabold my-4">
         Fitness Club
       </p>
@@ -17,13 +18,19 @@ const HeroBanner = () => {
       </p>
       <button
         type="button"
-        class="text-white bg-gradient-to-r  from-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        className="text-white bg-gradient-to-r  from-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
       >
         <a href="#exercises" className="text-xl">
           Explore Execrcises
         </a>
       </button>
 
+      {/* Blurry Exercise Text */}
+      <p className="text-6xl text-[#FF2625] opacity-10 text-[150px]">
+        Exercise
+      </p>
+
+      {/* Herobanner Image */}
       <img src={HeroBannerImage} alt="banner" id="hero-banner-img" />
     </div>
   );
--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  const [search, setSearch] = useState("");
+  const [exercises, setExercise] = useState([]);
+  const [bodyParts, setBodyParts] = useState([]);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const bodyPartData = await fetch(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
+
+      setBodyParts(["all", ...bodyPartData]);
+    };
+
+    fetchExercisesData();
+  }, []);
+
+  //Handling search
+  const handleSearch = async () => {
+    if (search) {
+      const exerciseData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions // For method and headers of API
+      );
+
+      //console.log(exerciseData);
+      const searchedExercises = exerciseData.filter(
+        (exercise) =>
+          exercise.name.toLowerCase().includes(search) ||
+          exercise.bodyPart.toLowerCase().includes(search) ||
+          exercise.equipment.toLowerCase().includes(search) ||
+          exercise.target.toLowerCase().includes(search)
+      );
+
+      setSearch(""); // emptied the search box
+      exercises(searchedExercises);
+    }
+  };
+
+  return (
+    <div className="mt-9 p-5 w-screen">
+      <p className="text-5xl mb-12 text-center">
+        Awsome Exercises You <br /> Should Know
+      </p>
+
+      {/* Searchbox */}
+      <div className="relative mb-18 flex justify-center">
+        <input
+          type="text"
+          placeholder="Search Exercises"
+          className="h-19 w-1/2 text-left text-xl border-none rounded-l-lg pl-4 p-3 outline-[#FF2625] bg-[#FF2625] bg-opacity-15 placeholder-slate-500"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value.toLowerCase());
+          }}
+        />
+        <button
+          className="bg-[#FF2625] p-3 w-40 rounded-r-lg text-xl text-white"
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default SearchExercises;
